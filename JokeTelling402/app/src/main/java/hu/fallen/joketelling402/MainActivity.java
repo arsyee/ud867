35b52f99bc2a9ab89a7eab997d2208eabcb992a4
arsyee <2476948+arsyee@users.noreply.github.com>
2018-04-29 15:39:13 +0200

    soluiton for 4.02

---
+++ JokeTelling402/app/src/main/java/hu/fallen/joketelling402/MainActivity.java
@@ -0,0 +1,17 @@
+package hu.fallen.joketelling402;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import hu.fallen.jokesmith.JokeSmith;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.tv_joke)).setText(JokeSmith.getJoke());
+    }
+}
---
+++ JokeTelling402/jokesmith/src/main/java/hu/fallen/jokesmith/JokeSmith.java
@@ -0,0 +1,7 @@
+package hu.fallen.jokesmith;
+
+public class JokeSmith {
+    public static final String getJoke() {
+        return "A. Smith walks into a bar. Yes, Agent Smith. From the Matrix. Not just a random smith...";
+    }
+}
